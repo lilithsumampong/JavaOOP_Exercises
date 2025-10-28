b45b997b8c69fda8fd1274142114f1e32022181d
lilithsumampong <lilithsumampong04@gmail.com>
2025-10-28 19:13:29 +0800

    Update PersonMain.java

--- src/Exer_1/PersonMain.java
+++ src/Exer_1/PersonMain.java
@@ -12,8 +12,8 @@ public class PersonMain {
     
     public static void main (String[] args) {
 
-        PersonMain person1 = new PersonMain("Connie", 19);
-        PersonMain person2 = new PersonMain("Carl", 15);
+        PersonMain person1 = new PersonMain("Lilith", 19);
+        PersonMain person2 = new PersonMain("JEJE", 15);
         
         System.out.println("I am " + person1.name + " and I am " + person1.age + " years old");
         System.out.println("I am " + person2.name + " and I am " + person2.age + " years old");
